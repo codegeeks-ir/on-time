6bdec925c427ab700d7223ff316a1d37ef6d1c74
Mohsen Farajollahi <gmohsenfarajollahi@gmail.com>
2025-11-14 03:13:20 +0330

    updating and installing workbox
    updating and installing workbox

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,21 @@ export default defineConfig({
         clientsClaim: true,
         cleanupOutdatedCaches: true,
         navigateFallback: '/index.html',
+        runtimeCaching: [
+          {
+            urlPattern: /\.(?:js|css|png|svg|woff2?|ttf|ico|xlsx)$/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'assets-cache',
+              expiration: {
+                maxEntries: 100,
+                maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+              },
+            },
+          },
+        ],
       },
+
       includeAssets: ['ontime192.png', 'ontime512.png'],
       manifest: {
         name: 'On-Time Bus time Tracker',
